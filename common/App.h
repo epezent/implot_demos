fe5e9bb0af3e147ce173845e705ff2792ae57c2f
Evan Pezent <epezent16@gmail.com>
2021-07-26 08:01:36 -0700

    avx2 tests

--- common/App.h
+++ common/App.h
@@ -7,6 +7,18 @@
 #include <string>
 #include <map>
 
+/// Macro to request high performance GPU in systems (usually laptops) with both
+/// dedicated and discrete GPUs
+#if defined(_WIN32) && defined(APP_USE_DGPU)
+    extern "C" __declspec(dllexport) unsigned long NvOptimusEnablement = 1;
+    extern "C" __declspec(dllexport) unsigned long AmdPowerXpressRequestHighPerformance = 1;
+#endif
+
+/// Macro to disable console on Windows
+#if defined(_WIN32) && defined(APP_NO_CONSOLE)
+    #pragma comment(linker, "/subsystem:windows /ENTRY:mainCRTStartup")
+#endif
+
 struct GLFWwindow;
 
 // Barebones Application Framework
