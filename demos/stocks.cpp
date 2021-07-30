1e0aac4d7d024b89f9d2e8bfa667f0c179f686e7
Evan Pezent <epezent16@gmail.com>
2021-07-29 18:42:15 -0700

    work on stocks demo

--- demos/stocks.cpp
+++ demos/stocks.cpp
@@ -10,10 +10,15 @@
 #include <iomanip>
 #include <sstream>
 #include <iostream>
+#include <filesystem>
 #include <stdio.h>
 #include <csv.h>
 #include <fmt/format.h>
 #include <implot_internal.h>
+
+
+
+namespace fs = std::filesystem;
 
 enum Interval
 {
@@ -22,12 +27,12 @@ enum Interval
     Interval_Monthly
 };
 
-class OHLC
+class TickerData
 {
 public:
 
-    OHLC() { }
-    OHLC(std::string ticker) : ticker(ticker) { }
+    TickerData() { }
+    TickerData(std::string ticker) : ticker(ticker) { }
 
     void reserve(int n) {
         time.reserve(n);
@@ -86,23 +91,30 @@ class YahooFinanceAPI
 public:
     YahooFinanceAPI() {}
 
-    OHLC download_ticker_data(std::string ticker, std::string start_date, std::string end_date, Interval interval)
+    TickerData get_ticker(std::string ticker, std::string start_date, std::string end_date, Interval interval)
     {
         std::transform(ticker.begin(), ticker.end(),ticker.begin(), ::toupper);
 
         std::string url = build_url(ticker, start_date, end_date, interval);
-        std::string output_file_name = ticker + "_" + start_date + "_" + end_date + ".csv";
-        download_file(url, output_file_name);        
+        std::string filename = ticker + "_" + start_date + "_" + end_date + ".csv";
 
-        OHLC ohlc(ticker);
-        io::CSVReader<6> in(output_file_name);
+        if (!fs::exists(filename)) {
+            fmt::print("Downloading {}\n",filename);
+            download_file(url, filename);  
+        }      
+        else {
+            fmt::print("Reloading {}\n",filename);
+        }
+
+        TickerData data(ticker);
+        io::CSVReader<6> in(filename);
         in.read_header(io::ignore_extra_column, "Date", "Open", "High", "Low", "Close", "Volume");
         std::string date; double o; double h; double l; double c; double v;
         while(in.read_row(date, o, h, l, c, v)){
             double t = timestamp_from_string(date);
-            ohlc.push_back(t,o,h,l,c,v);
+            data.push_back(t,o,h,l,c,v);
         }
-        return ohlc;
+        return data;
     }
 
 private:
@@ -170,60 +182,63 @@ int BinarySearch(const T* arr, int l, int r, T x) {
     return -1;
 }
 
-void PlotCandlestick(const char* label_id, const double* xs, const double* opens, const double* closes, const double* lows, const double* highs, int count, bool tooltip = true, float width_percent = 0.5f, ImVec4 bullCol = ImVec4(0,1,0,1), ImVec4 bearCol = ImVec4(1,0,0,1)) {
-
-    // get ImGui window DrawList
+void TickerTooltip(const TickerData& data, bool span_subplots = false) {
     ImDrawList* draw_list = ImPlot::GetPlotDrawList();
-    // calc real value width
-
-    double half_width = 24*60*60 * width_percent / 2;
-
-    // custom tool
-    if (ImPlot::IsPlotHovered() && tooltip) {
+    const double half_width = 24*60*60*0.25*1.5;
+    const bool hovered = span_subplots ? ImPlot::IsSubplotsHovered() : ImPlot::IsPlotHovered();
+    if (hovered) {
         ImPlotPoint mouse   = ImPlot::GetPlotMousePos();
         mouse.x             = ImPlot::RoundTime(ImPlotTime::FromDouble(mouse.x), ImPlotTimeUnit_Day).ToDouble();
-        float  tool_l       = ImPlot::PlotToPixels(mouse.x - half_width * 1.5, mouse.y).x;
-        float  tool_r       = ImPlot::PlotToPixels(mouse.x + half_width * 1.5, mouse.y).x;
+        float  tool_l       = ImPlot::PlotToPixels(mouse.x - half_width, mouse.y).x;
+        float  tool_r       = ImPlot::PlotToPixels(mouse.x + half_width, mouse.y).x;
         float  tool_t       = ImPlot::GetPlotPos().y;
         float  tool_b       = tool_t + ImPlot::GetPlotSize().y;
         ImPlot::PushPlotClipRect();
         draw_list->AddRectFilled(ImVec2(tool_l, tool_t), ImVec2(tool_r, tool_b), IM_COL32(128,128,128,64));
         ImPlot::PopPlotClipRect();
         // find mouse location index
-        int idx = BinarySearch(xs, 0, count - 1, mouse.x);
+        int idx = BinarySearch(data.time.data(), 0, data.size() - 1, mouse.x);
         // render tool tip (won't be affected by plot clip rect)
-        if (idx != -1) {
+        if (ImPlot::IsPlotHovered() && idx != -1) {
             ImGui::BeginTooltip();
             char buff[32];
-            ImPlot::FormatDate(ImPlotTime::FromDouble(xs[idx]),buff,32,ImPlotDateFmt_DayMoYr,ImPlot::GetStyle().UseISO8601);
-            ImGui::Text("Date:");  ImGui::SameLine(50); ImGui::Text("%s",  buff);
-            ImGui::Text("Open:");  ImGui::SameLine(50); ImGui::Text("$%.2f", opens[idx]);
-            ImGui::Text("Close:"); ImGui::SameLine(50); ImGui::Text("$%.2f", closes[idx]);
-            ImGui::Text("Low:");   ImGui::SameLine(50); ImGui::Text("$%.2f", lows[idx]);
-            ImGui::Text("High:");  ImGui::SameLine(50); ImGui::Text("$%.2f", highs[idx]);
+            ImPlot::FormatDate(ImPlotTime::FromDouble(data.time[idx]),buff,32,ImPlotDateFmt_DayMoYr,ImPlot::GetStyle().UseISO8601);
+            ImGui::Text("Date:");   ImGui::SameLine(60); ImGui::Text("%s",  buff);
+            ImGui::Text("Open:");   ImGui::SameLine(60); ImGui::Text("$%.2f", data.open[idx]);
+            ImGui::Text("Close:");  ImGui::SameLine(60); ImGui::Text("$%.2f", data.close[idx]);
+            ImGui::Text("High:");   ImGui::SameLine(60); ImGui::Text("$%.2f", data.high[idx]);
+            ImGui::Text("Low:");    ImGui::SameLine(60); ImGui::Text("$%.2f", data.low[idx]);
+            ImGui::Text("Volume:"); ImGui::SameLine(60); ImGui::Text(fmt::format(std::locale("en_US.UTF-8"),"{:L}", (int)(data.volume[idx]*1000000)).c_str());
             ImGui::EndTooltip();
         }
     }
+}
 
+void PlotOHLC(const char* label_id, const TickerData& data, ImVec4 bullCol = ImVec4(0,1,0,1), ImVec4 bearCol = ImVec4(1,0,0,1)) {
+
+    // get ImGui window DrawList
+    ImDrawList* draw_list = ImPlot::GetPlotDrawList();
+    // calc real value width
+    const double half_width = 24*60*60*0.25;
     // begin plot item
     if (ImPlot::BeginItem(label_id)) {
         // override legend icon color
         ImPlot::GetCurrentItem()->Color = ImGui::GetColorU32(bullCol);
         // fit data if requested
         if (ImPlot::FitThisFrame()) {
-            for (int i = 0; i < count; ++i) {
-                ImPlot::FitPoint(ImPlotPoint(xs[i], lows[i]));
-                ImPlot::FitPoint(ImPlotPoint(xs[i], highs[i]));
+            for (int i = 0; i < data.size(); ++i) {
+                ImPlot::FitPoint(ImPlotPoint(data.time[i], data.low[i]));
+                ImPlot::FitPoint(ImPlotPoint(data.time[i], data.high[i]));
             }
         }
         // render data
-        for (int i = 0; i < count; ++i) {
-            ImU32 color      = ImGui::GetColorU32(opens[i] > closes[i] ? bearCol : bullCol);
-            ImVec2 open_pos  = ImPlot::PlotToPixels(xs[i] - half_width, opens[i]);
-            ImVec2 close_pos = ImPlot::PlotToPixels(xs[i] + half_width, closes[i]);
+        for (int i = 0; i < data.size(); ++i) {
+            ImU32 color      = ImGui::GetColorU32(data.open[i] > data.close[i] ? bearCol : bullCol);
+            ImVec2 open_pos  = ImPlot::PlotToPixels(data.time[i] - half_width, data.open[i]);
+            ImVec2 close_pos = ImPlot::PlotToPixels(data.time[i] + half_width, data.close[i]);
             draw_list->AddRectFilled(open_pos, close_pos, color);   
-            ImVec2 low_pos   = ImPlot::PlotToPixels(xs[i], lows[i]);
-            ImVec2 high_pos  = ImPlot::PlotToPixels(xs[i], highs[i]);
+            ImVec2 low_pos   = ImPlot::PlotToPixels(data.time[i], data.low[i]);
+            ImVec2 high_pos  = ImPlot::PlotToPixels(data.time[i], data.high[i]);
             draw_list->AddLine(low_pos,  high_pos , color, ImMax(1.0f, ImAbs(open_pos.x - close_pos.x)/10.0f));
         }
 
@@ -238,7 +253,7 @@ struct ImStocks : App
     using App::App;
 
     void init() override {
-        m_ohlc.push_back(m_api.download_ticker_data("FB", "2016-07-01", "2021-07-01", Interval_Daily));
+        m_ticker_data.push_back(m_api.get_ticker("AAPL", "2016-07-01", "2021-07-01", Interval_Daily));
         ImPlot::GetStyle().FitPadding.y = 0.2f;
     }
 
@@ -261,50 +276,52 @@ struct ImStocks : App
         // }
 
 
-        static char buff[8] = "FB";
+        static char buff[8] = "AAPL";
         if (ImGui::InputText("Ticker",buff,8,ImGuiInputTextFlags_EnterReturnsTrue|ImGuiInputTextFlags_CharsUppercase)) {
-            m_ohlc.push_back(m_api.download_ticker_data(buff, "2016-07-01", "2021-07-01", Interval_Daily));
+            m_ticker_data.push_back(m_api.get_ticker(buff, "2016-07-01", "2021-07-01", Interval_Daily));
         }
 
+    
         if (ImGui::BeginTabBar("TickerTabs")) {
-            for (auto& ohlc : m_ohlc) {
-                if (ImGui::BeginTabItem(ohlc.ticker.c_str())) {               
+            for (auto& data : m_ticker_data) {
+                if (ImGui::BeginTabItem(data.ticker.c_str())) {               
                     static float ratios[] = {2,1};
                     if (ImPlot::BeginSubplots("##Stocks",2,1,ImVec2(-1,-1),ImPlotSubplotFlags_LinkCols,ratios)) {
-                        ImPlot::SetNextPlotLimitsX(ohlc.time[0],ohlc.time.back());
-                        ImPlot::SetNextPlotFormatY("$%.2f");
-                        if (ImPlot::BeginPlot("##OhlcPlot",0,0,ImVec2(0,0),0,ImPlotAxisFlags_Time|ImPlotAxisFlags_NoTickLabels,ImPlotAxisFlags_AutoFit|ImPlotAxisFlags_RangeFit)) {
+                        ImPlot::SetNextPlotLimitsX(data.time[0],data.time.back());
+                        ImPlot::SetNextPlotFormatY("$%.0f");
+                        if (ImPlot::BeginPlot("##dataPlot",0,0,ImVec2(0,0),0,ImPlotAxisFlags_Time|ImPlotAxisFlags_NoTickLabels,ImPlotAxisFlags_AutoFit|ImPlotAxisFlags_RangeFit)) {
+                            TickerTooltip(data, true);                            
                             ImPlot::SetNextFillStyle(ImVec4(0.5,0.5,1,1),0.25f);
-                            ImPlot::PlotShaded("BB",ohlc.time.data(),ohlc.bollinger_top.data(),ohlc.bollinger_bot.data(),ohlc.size());
+                            ImPlot::PlotShaded("BB",data.time.data(),data.bollinger_top.data(),data.bollinger_bot.data(),data.size());
                             ImPlot::SetNextLineStyle(ImVec4(0.5,0.5,1,1));
-                            ImPlot::PlotLine("BB",ohlc.time.data(),ohlc.bollinger_mid.data(),ohlc.size());
-                            PlotCandlestick("OHLC",ohlc.time.data(),ohlc.open.data(),ohlc.close.data(),ohlc.low.data(),ohlc.high.data(),ohlc.size(),true,0.5f,bull_col,bear_col);                
+                            ImPlot::PlotLine("BB",data.time.data(),data.bollinger_mid.data(),data.size());
+                            PlotOHLC("OHLC",data,bull_col,bear_col);                
                             ImPlot::HideNextItem(true);
                             ImPlot::SetNextLineStyle(ImVec4(1,1,1,1));
-                            ImPlot::PlotLine("Close",ohlc.time.data(),ohlc.close.data(),ohlc.size());
-
+                            ImPlot::PlotLine("Close",data.time.data(),data.close.data(),data.size());
                             ImPlot::EndPlot();
                         }
-                        ImPlot::SetNextPlotLimitsX(ohlc.time[0],ohlc.time.back());
+                        ImPlot::SetNextPlotLimitsX(data.time[0],data.time.back());
                         ImPlot::SetNextPlotFormatY("%.0fM");
                         if (ImPlot::BeginPlot("##VolumePlot",0,0,ImVec2(0,0),0,ImPlotAxisFlags_Time,ImPlotAxisFlags_AutoFit|ImPlotAxisFlags_RangeFit)) {
+                            TickerTooltip(data, true);
                             ImPlot::SetNextFillStyle(ImVec4(1.f,0.75f,0.25f,1));
-                            ImPlot::PlotBars("Volume",ohlc.time.data(),ohlc.volume.data(),ohlc.size(),60*60*24*0.5);
+                            ImPlot::PlotBars("Volume",data.time.data(),data.volume.data(),data.size(),60*60*24*0.5);
                             ImPlot::EndPlot();
                         }
                         ImPlot::EndSubplots();
                     }
-
                     ImGui::EndTabItem();
                 }
             }            
             ImGui::EndTabBar();    
         }
+        
         ImGui::End();
     }
 
     YahooFinanceAPI   m_api;
-    std::vector<OHLC> m_ohlc;
+    std::vector<TickerData> m_ticker_data;
 };
 
 int main(int argc, char const *argv[])
